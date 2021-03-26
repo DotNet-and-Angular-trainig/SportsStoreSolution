2d605d809860f2b9902d1adabf25b9265819add5
Maheshkumar N <pn.maheshkumar@gmail.com>
2021-03-26 17:58:20 +0530

    MAster

--- SportsStoreApp/Startup.cs
+++ SportsStoreApp/Startup.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -9,82 +8,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using SportsStoreApp.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using SportsStoreApp.Models.Concrete;
-using SportsStoreApp.Models.Abstract;
-using Microsoft.AspNetCore.Routing;
-
 namespace SportsStoreApp
 {
   public class Startup
   {
-    public IConfiguration Configuration { get; }
-    public Startup(IConfiguration configuration)
-    {
-      Configuration = configuration;
-    }
-
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddMvc();
-
-      services.AddDbContext<SportsStoreDbContext>(cfg =>
-      {
-        cfg.UseSqlServer(Configuration["ConnectionStrings:SportsStoreConnection"], sqlServerOptionsAction: sqlOptions =>
-        {
-          sqlOptions.EnableRetryOnFailure(maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
-        });
-        cfg.UseLoggerFactory(LoggerFactory.Create(cfg => { cfg.AddConsole(); })).EnableSensitiveDataLogging();
-      });
-
-      services.AddScoped<IProductRepository, EFProductRepository>();
-      services.AddScoped<IOrderRepository, EFOrderRepository>();
-      services.AddScoped<IOrderDetailRepository, EFOrderDetailRepository>();
-
-      services.AddSwaggerGen(cfg => {
-        cfg.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title ="SportsStore", Version = "v1" });
-      });
     }
 
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
       }
 
-      app.UseSwagger();
-      app.UseSwaggerUI(cfg => {
-        cfg.SwaggerEndpoint("/swagger/v1/swagger.json", "SportsStore v1");
-      });
-
-      using (var scope = app.ApplicationServices.CreateScope())
-      {
-        SportsStoreDbContext context = scope.ServiceProvider.GetRequiredService<SportsStoreDbContext>();// will get the SportsStoreDbContext object
-        var createDatabase = context.Database.EnsureCreated();
-        if (createDatabase)
-        {
-          SportsStoreSeedData.PopulateSportsStore(context);
-          logger.LogInformation($"***SportsStoreSeedData Called, '{context.Products.Count()}' - Products Added\n'{context.Orders.Count()}' - Orders Added\n'{context.OrderDetails.Count()}' - OrderDetails Added***");
-        }
-      }
-
       app.UseRouting();
 
-      app.UseEndpoints(ConfigureRoutes);
-    }
-
-    private void ConfigureRoutes(IEndpointRouteBuilder routerBuilder)
-    {
-      routerBuilder.MapControllers();
-      routerBuilder.MapGet("/", async context =>
+      app.UseEndpoints(endpoints =>
       {
-        await context.Response.WriteAsync("<div style='background-color:Cornflowerblue; text-align: center; color: White;'>" +
-          "<h1>Sports Store Site Under Construction</h1>" +
-          "</div>");
+        endpoints.MapGet("/", async context =>
+              {
+            await context.Response.WriteAsync("Hello World!");
+          });
       });
     }
   }
 }
+
