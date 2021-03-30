27beb55c4ad1c95a1b340365fa6939c35399e94c
Maheshkumar N <pn.maheshkumar@gmail.com>
2021-03-30 17:39:06 +0530

    Stores Module

--- SportsStoreApp/ClientApp/src/app/app.module.ts
+++ SportsStoreApp/ClientApp/src/app/app.module.ts
@@ -6,19 +6,19 @@ import { AppComponent } from './app.component';
 // Both the components are added to a single module
 //import { NavHeaderComponent } from './nav/navheader.component';
 //import { NavFooterComponent } from './nav/navfooter.component';
+//import { StoreComponent } from './stores/store.component';
 
 import { NavModule } from './nav/nav.module';
+import { StoresModule } from './stores/stores.module';
 
-import { StoreComponent } from './stores/store.component';
 
 @NgModule({
   declarations: [
     AppComponent
-    //NavHeaderComponent, NavFooterComponent
-    , StoreComponent
+    //NavHeaderComponent, NavFooterComponent, StoreComponent
   ],
   imports: [
-    BrowserModule, NavModule
+    BrowserModule, NavModule, StoresModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ SportsStoreApp/ClientApp/src/app/stores/stores.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { StoreComponent } from './store.component';
+
+@NgModule({
+  imports: [],
+  declarations: [StoreComponent],
+  exports: [StoreComponent]
+})
+
+export class StoresModule { }
