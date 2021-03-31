3740809726c4071b138ff66ed90350e5e6bebd96
Maheshkumar N <pn.maheshkumar@gmail.com>
2021-03-31 12:30:27 +0530

    Paging

--- SportsStoreApp/ClientApp/src/app/stores/store.component.ts
+++ SportsStoreApp/ClientApp/src/app/stores/store.component.ts
@@ -10,11 +10,15 @@ import { ProductRepository } from '../models/product.repository';
 
 export class StoreComponent {
   public selectedCategory: string = null!;
+  public productsPerPage: number = 4;
+  public selectedPage: number = 1;
 
   constructor(private productRepository: ProductRepository) { }
 
   get products(): Product[] {
-    return this.productRepository.getProducts(this.selectedCategory);// with category
+    const pageIndex = ((this.selectedPage - 1) * this.productsPerPage);
+    return this.productRepository.getProducts(this.selectedCategory).splice(pageIndex, this.productsPerPage);
+    // return this.productRepository.getProducts(this.selectedCategory);// with category
     // return this.productRepository.getProducts();// First
   }
 
@@ -26,6 +30,20 @@ export class StoreComponent {
 
   addProductToCart(product: Product) {
     console.log(`Selected Product will be added to the Cart: \n${JSON.stringify(product)}`);
+  }
+
+  changePage(newPage: number) { this.selectedPage = newPage; }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = newSize;
+    this.changePage(1);
+  }
+
+  // this will return a array. array of numbers rather than the number itself (solution)
+  get pageNumbers(): number[] {
+    const result = Array(Math.ceil(this.productRepository.getProducts(this.selectedCategory).length / this.productsPerPage)).fill(0).map((x, i) => i + 1);
+    console.log(`pageNumber: ${result}`);
+    return result;
   }
 
 }
